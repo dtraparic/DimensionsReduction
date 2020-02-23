e6a04f4de1063bfb6d93d7fb136a32ebcfe0b7ef
julien <julien.choveton@gmail.com>
2020-02-24 00:24:14 +0100

    images

--- Julien/pcaknn.py
+++ Julien/pcaknn.py
@@ -11,12 +11,14 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
 from sklearn import datasets
+from sklearn.metrics import precision_score
 from skimage import exposure
 import numpy as np
 import imutils
 import cv2
 import sklearn
 from sklearn import manifold, decomposition, discriminant_analysis
+
 import time
 
  
@@ -33,76 +35,88 @@ mnist = datasets.load_digits()
 
 (trainData1, testData1, trainLabels1, testLabels1) = train_test_split(np.array(mnist.data),
 	mnist.target, test_size=0.25, random_state=42)
-#
-#X = mnist.data
-#y = mnist.target
-#n_samples, n_features = X.shape
-#
-#
+
+X = mnist.data
+y = mnist.target
+n_samples, n_features = X.shape
+
+
 #X_pca = decomposition.PCA(n_components=3).fit_transform(X)    
-#   
-##X_lda = discriminant_analysis.LinearDiscriminantAnalysis(n_components=3).fit_transform(X, y)
-# 
-#mnist.data = X_pca
 
+values = []
+x = range(1,64,1)
+for i in range(1,64,1):    
+    #X_lda = discriminant_analysis.LinearDiscriminantAnalysis(n_components=i).fit_transform(X, y) 
+    #X_lda = discriminant_analysis.LinearDiscriminantAnalysis(n_components=3).fit_transform(X, y)
+     
+    X_pca = decomposition.PCA(n_components=i).fit_transform(X)    
 
-
-
-# take the MNIST data and construct the training and testing split, using 75% of the
-# data for training and 25% for testing
-(trainData, testData, trainLabels, testLabels) = train_test_split(np.array(mnist.data),
-	mnist.target, test_size=0.25, random_state=42)
- 
-# now, let's take 10% of the training data and use that for validation
-(trainData, valData, trainLabels, valLabels) = train_test_split(trainData, trainLabels,
-	test_size=0.1, random_state=84)
- 
-# show the sizes of each data split
-print("training data points: {}".format(len(trainLabels)))
-print("validation data points: {}".format(len(valLabels)))
-print("testing data points: {}".format(len(testLabels)))
-# initialize the values of k for our k-Nearest Neighbor classifier along with the
-# list of accuracies for each value of k
-kVals = range(1, 30, 2)
-accuracies = []
- 
-# loop over various values of `k` for the k-Nearest Neighbor classifier
-for k in range(1, 30, 2):
-	# train the k-Nearest Neighbor classifier with the current value of `k`
-	model = KNeighborsClassifier(n_neighbors=k)
-	model.fit(trainData, trainLabels)
- 
-	# evaluate the model and update the accuracies list
-	score = model.score(valData, valLabels)
-	#print("k=%d, accuracy=%.2f%%" % (k, score * 100))
-	accuracies.append(score)
- 
-# find the value of k that has the largest accuracy
-i = int(np.argmax(accuracies))
-print("k=%d achieved highest accuracy of %.2f%% on validation data" % (kVals[i],
-	accuracies[i] * 100))
-
-
-
-
-# re-train our classifier using the best k value and predict the labels of the
-# test data
-
-t = time.time()
-model = KNeighborsClassifier(n_neighbors=kVals[i])
-model.fit(trainData, trainLabels)
-elapsed = time.time() - t
-
-print(elapsed)
-
-
-#predictions = model.predict(testData)
-# 
-## show a final classification report demonstrating the accuracy of the classifier
-## for each of the digits
-#print("EVALUATION ON TESTING DATA")
-#print(classification_report(testLabels, predictions))
-#
+    mnist.data = X_pca
+    
+    
+    
+    # take the MNIST data and construct the training and testing split, using 75% of the
+    # data for training and 25% for testing
+    (trainData, testData, trainLabels, testLabels) = train_test_split(np.array(mnist.data),
+    	mnist.target, test_size=0.25, random_state=42)
+     
+    # now, let's take 10% of the training data and use that for validation
+    (trainData, valData, trainLabels, valLabels) = train_test_split(trainData, trainLabels,
+    	test_size=0.5, random_state=84)
+     
+    # show the sizes of each data split
+#    print("training data points: {}".format(len(trainLabels)))
+#    print("validation data points: {}".format(len(valLabels)))
+#    print("testing data points: {}".format(len(testLabels)))
+    # initialize the values of k for our k-Nearest Neighbor classifier along with the
+    # list of accuracies for each value of k
+    kVals = range(1, 30, 2)
+    accuracies = []
+     
+    # loop over various values of `k` for the k-Nearest Neighbor classifier
+    for k in range(1, 30, 2):
+    	# train the k-Nearest Neighbor classifier with the current value of `k`
+    	model = KNeighborsClassifier(n_neighbors=k)
+    	model.fit(trainData, trainLabels)
+     
+    	# evaluate the model and update the accuracies list
+    	score = model.score(valData, valLabels)
+    	#print("k=%d, accuracy=%.2f%%" % (k, score * 100))
+    	accuracies.append(score)
+     
+    # find the value of k that has the largest accuracy
+    i = int(np.argmax(accuracies))
+#    print("k=%d achieved highest accuracy of %.2f%% on validation data" % (kVals[i],
+#    	accuracies[i] * 100))
+    
+    
+    
+    
+    # re-train our classifier using the best k value and predict the labels of the
+    # test data
+    
+    t = time.time()
+    model = KNeighborsClassifier(n_neighbors=kVals[i])
+    model.fit(trainData, trainLabels)
+    elapsed = time.time() - t
+    
+    #print(elapsed)
+    
+    
+    predictions = model.predict(testData)
+     
+    # show a final classification report demonstrating the accuracy of the classifier
+    # for each of the digits
+#    print("EVALUATION ON TESTING DATA")
+    b = precision_score(testLabels, predictions, average='weighted')
+    values.append(b)
+    a = classification_report(testLabels, predictions)
+#    print(b)
+#    print(a)
+    
+    
+plt.plot(x,values)
+plt.yticks(np.arange(0.4, 1.05, 0.05))
 #
 #	
 ## loop over a few random digits
